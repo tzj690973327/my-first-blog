a6a997fad0ca9747c30e53fd3685177468745f64
tzj690973327 <shaq0621@outlook.com>
2024-11-19 16:11:18 +0900

    update

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 router=routers.DefaultRouter()
 router.register('Post', views.BlogImages)
@@ -14,4 +16,5 @@ urlpatterns = [
     path('api_root/', include(router.urls)),
     path('', include(router.urls)),
     path('api/', include('rest_framework.urls')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-qp2-c#l1qay!^r-%4*__g#$y(=dg(al-x&$$g8_v-=uj2(-xz6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tzjzk.pythonanywhere.com', '127.0.0.1','10.0.2.2']
+ALLOWED_HOSTS = ['tzjzk.pythonanywhere.com', '127.0.0.1','10.0.2.2', 'localhost']
 
 # Application definition
 
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
